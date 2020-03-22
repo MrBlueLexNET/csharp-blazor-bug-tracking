5e9b9995fd71789fc767193f69f047e290712b27
Alex Wolf <alexrudiwolf@gmail.com>
2020-03-22 19:13:08 -0400

    initial commit

---
+++ BugTrackerUI/IBugService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTrackerUI
+{
+    public interface IBugService
+    {
+        List<Bug> GetBugs();
+
+        void AddBug(Bug newBug);
+    }
+}
---
+++ BugTrackerUI/Services/BugService.cs
@@ -0,0 +1,23 @@
+﻿using BugTrackerUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTrackerUI.Services
+{
+    public class BugService : IBugService
+    {
+        private List<Bug> Bugs = new List<Bug>();
+
+        public void AddBug(Bug newBug)
+        {
+            Bugs.Add(newBug);
+        }
+
+        public List<Bug> GetBugs()
+        {
+            return Bugs;
+        }
+    }
+}
