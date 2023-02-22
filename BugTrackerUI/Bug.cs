0821bb6ee4f22c1040c7383ddfed717b2233baf5
JYM <jeanyvesmichel@yahoo.com>
2023-02-22 11:36:34 +0100

    M5_BugTrackerUI.Tests.AddDataValidationToForm Tests in group: 4 all Passed

--- BugTrackerUI/Bug.cs
+++ BugTrackerUI/Bug.cs
@@ -9,11 +9,13 @@ namespace BugTrackerUI
     public class Bug
     {
         public int Id { get; set; }
-
+        [Required]
         public string Title { get; set; }
-
+        [Required]
+        [MinLength(length: 10)]
         public string Description { get; set; }
-
+        [Required]
+        [Range(minimum: 1, maximum: 5)]
         public int Priority { get; set; }
     }
 }
