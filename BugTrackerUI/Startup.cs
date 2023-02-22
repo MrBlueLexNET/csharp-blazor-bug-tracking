88827f87aae9598c9a2271b12db3d896c707ee7a
JYM <jeanyvesmichel@yahoo.com>
2023-02-22 11:12:18 +0100

    BugService Added

--- BugTrackerUI/Startup.cs
+++ BugTrackerUI/Startup.cs
@@ -28,6 +28,7 @@ namespace BugTrackerUI
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddSingleton<IBugService, BugService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
